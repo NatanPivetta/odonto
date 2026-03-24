98a0d7e09c7eb80946c8dca4521b516c4bfd8fd2
natanpivetta <natanpivetta@southsystem.com.br>
2026-03-24 14:24:13 -0300

    Adicionada tela de atividades e modal de criação de atividades. Adicionados dados mockados e contexto de usuário por role

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { DM_Sans, DM_Serif_Display } from 'next/font/google'
+import { AuthProvider } from '@/lib/mock/auth'
 import './globals.css'
 
 const dmSans = DM_Sans({
@@ -22,8 +23,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="pt-BR" className={`${dmSans.variable} ${dmSerifDisplay.variable} dark`}>
-        <body>{children}</body>
+        <html lang="pt-BR" className={`${dmSans.variable} ${dmSerifDisplay.variable}`}>
+        <body>
+        <AuthProvider>
+            {children}
+        </AuthProvider>
+        </body>
         </html>
     )
 }
